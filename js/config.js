00652c6a5b876d08d13e8c16f15ae4a0baf470ae
tarodoteko0306 <yusei19941220@gmail.com>
2025-08-23 02:24:53 +0900

    Add EmailJS configuration for automatic email notifications

---
+++ js/config.js
@@ -0,0 +1,18 @@
+// Stripe設定
+const STRIPE_CONFIG = {
+    publishableKey: 'pk_live_51RvKWh673l6kt5LxH6Tyz46zGR5KbC9JkWvD5UR0Tkt0Ofobap7qptE8OkPVLFo08KvqbcEwy4T1l96k3xAVaO00vqUtav39',
+    webhookEndpoint: 'https://script.google.com/macros/s/AKfycbypzzmH-SsoK8urIOLJfV6msGEiQo53GcRSYPBioO865VYXf3aHsffYl8GwyoBjuWZgGQ/exec'
+};
+
+// Google Spreadsheet設定
+const SPREADSHEET_CONFIG = {
+    spreadsheetId: '1yfQzbzw5QHRvZt3CvLxukTjJtZJav3nshNaY4Fxl8z8',
+    apiUrl: 'https://script.google.com/macros/s/AKfycbypzzmH-SsoK8urIOLJfV6msGEiQo53GcRSYPBioO865VYXf3aHsffYl8GwyoBjuWZgGQ/exec'
+};
+
+// EmailJS設定
+const EMAIL_CONFIG = {
+    serviceId: 'service_l3e47bc',
+    templateId: 'template_bzk9mpf',
+    userId: 'bJTa6D-K4-MainZRn'
+};
